fc161695b6d40b68aad1bc1a581e6899adaeffc4
Sang Eun <ks60383@naver.com>
2014-12-07 16:39:09 +0900

    register & studentinfo 수정필요

--- register.java
+++ register.java
@@ -6,41 +6,41 @@ import java.util.*;
 public class register {
 
 public static void main(String[] args) {
-	
-	
-	ArrayList<StudentInfo> stdList = new ArrayList<StudentInfo>();
-	String id;
-	String num;
-	String pw;
-	final int ID=1,NUM=2,PW=3;
-	
-	File aFile = new File("log.txt");
-	FileReader fileReader;
-	try {
-		fileReader = new FileReader(aFile);
-		BufferedReader reader = new BufferedReader(fileReader);
 		
-		String line = null;
+		ArrayList<StudentInfo> stdList = new ArrayList<StudentInfo>();
+		String id;
+		String num;
+		String pw;
+		final int ID=1,NUM=2,PW=3;
 		
+		File aFile = new File("log.txt");
+		FileReader fileReader;
 		try {
-			while((line = reader.readLine()) != null){
-				id = line.substring(0, 4);
-				num = line.substring(4,11);
-				pw = line.substring(11,15);
-				StudentInfo std = new StudentInfo(id, num, pw);
-				stdList.add(std);
+			fileReader = new FileReader(aFile);
+			BufferedReader reader = new BufferedReader(fileReader);
+			
+			String line = null;
+			
+			try {
+				while((line = reader.readLine()) != null){
+					id = line.substring(0, 4);
+					num = line.substring(4,11);
+					pw = line.substring(11,15);
+					StudentInfo std = new StudentInfo(id, num, pw);
+					stdList.add(std);
+				}
+				reader.close();
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
 			}
-			reader.close();
-		} catch (IOException e) {
+			
+		} catch (FileNotFoundException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 		
-	} catch (FileNotFoundException e) {
-		// TODO Auto-generated catch block
-		e.printStackTrace();
-	}
-	
+
 		Scanner scan = new Scanner(System.in);
 		String input = null;
 		StudentInfo std = new StudentInfo();
@@ -83,6 +83,29 @@ public static void main(String[] args) {
 				e1.printStackTrace();
 			}
 		
+		
+	}
 	
+	public static boolean dupCheck(String target,int flag,ArrayList<StudentInfo> List){
+		//1.id 2.num 3.pw
+		switch(flag){
+		case 1:
+			for(int i=0; i < List.size(); i++){
+				if(target.equals(List.get(i).getStuId()))
+					return false;
+			}
+		case 2:
+			for(int i=0; i < List.size(); i++){
+			if(target.equals(List.get(i).getStuNum()))
+				return false;
+		}
+		case 3:
+			for(int i=0; i < List.size(); i++){
+			if(target.equals(List.get(i).getStupw()))
+				return false;
+		}
+		}
+		
+		return true;
 	}
 }
